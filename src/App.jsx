6c79b7c7cd75ecfe136cc94985a8ece5a3c1d554
El-HobbitCR <jefh15@hotmail.com>
2022-05-21 19:04:12 -0600

    creo dos estados para poder crear las tareas uno es un estadopara poder agregar las tareas individualmente y el otro para agregar las tareas lo cual es un estado de tipo array para agregar todas las tareas. Ademas uso nanoid() para poder hacer los id de mis tareas aleatorios

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,40 @@
+import { nanoid } from "nanoid";
+import { useState } from "react";
 
 
 function App() {
 
+  const [tarea, setTarea] = useState('')
+  const [tareas, setTareas] = useState([])
 
 
+  // funcion de flecha, recibo un evento
+  const agregarTarea = (e) => {
+    // evita que se procese el formulario
+    e.preventDefault()
 
+    // valido que tenga algun texto
+    if (!tarea.trim()) {
+      console.log('Campo vacio')
+      // para que se salga
+      return
+    }
+    console.log(tarea)
+
+    // agregamos todas las tarea
+    setTareas(
+      // porque es un array
+      [
+        // uso el operador de propagacion para hacer una copia del array
+        ...tareas,
+        // nanoid() ---> nanoid() nos genera un id aleatorio
+        { id: nanoid(), nombreTarea: tarea }
+      ]
+    )
+
+    // limpia el input
+    setTarea('')
+  }
 
   return (
     <div className="container mt-5">
@@ -33,17 +63,27 @@ function App() {
             FORMULARIO
           </h4>
           {/* Hago el formulario */}
-          <form>
+          <form onSubmit={agregarTarea}>
             {/* input.form-control.mb-2 */}
             <input
+              // el tipo
               type="text"
+              // clase
               className="form-control mb-2"
-              placeholder="Ingrese Tarea" />
+              // que dice dentro mientras no hay nada escrito
+              placeholder="Ingrese la tarea"
+              // evento que ayuda a recibir lo escribimos en el input
+              // relacionamos el input con le estado
+              onChange={(e) => setTarea(e.target.value)}
+              // pasamos el estado para que se pueda limpiar
+              value={tarea}
+            />
             {/* button.btn.btn-dark.btn-block */}
             <button
               className="btn btn-dark btn-block"
               // para que funcione con la tecla enter
               type="submit">Agregar</button>
+
           </form>
         </div>
       </div>
